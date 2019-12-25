54e9bd0e812b398e40794086827775e2a6bbdc99
Héricles Emanuel <hericles.me@gmail.com>
2019-12-25 10:09:41 -0300

    update server start message

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.post((process.env.ENDPOINT || '/chat'), async (req, res) => {
 
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+  console.log(`Crocubot API Running → PORT ${server.address().port}`);
 })
